5b0966a4e5141e68f925fec5f0148d763b6ed1c9
David Oparah <davidoparah@gmail.com>
2024-04-10 21:36:15 +0100

    commit 3

--- main.tf
+++ main.tf
@@ -14,3 +14,20 @@ resource "digitalocean_ssh_key" "prod_ssh_key" {
   name   = "${var.app_name}-ssh-key"
   public_key = file("${var.ssh_key}.pub")
 }
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "manager_node" {
+  name     = "${var.app_name}-manager-droplet"
+  image    = "${var.do_image}"
+  region   = "${var.do_region}"
+  size     = "${var.do_size}"
+  ssh_keys = [var.ssh_fingerprint]
+}
+
+resource "digitalocean_droplet" "worker1_node" {
+  name     = "${var.app_name}-worker1-droplet"
+  image    = "${var.do_image}"
+  region   = "${var.do_region}"
+  size     = "${var.do_size}"
+  ssh_keys = [var.ssh_fingerprint]
+}
